48dc958c69fd13843eb376f4982780718e7783e1
minhtiep <quangvantiepp@gmail.com>
2024-03-31 22:02:32 +0700

    update home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { HomeStyles as styles } from "./Styles";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Home </h1>
+      <div>
+        <video width={400} height={400} autoPlay loop muted>
+          <source
+            src="https://d75afklyimq5v.cloudfront.net/atheros-learning/web-assets/platform-carousel-light.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/home/Styles.tsx
@@ -0,0 +1,7 @@
+import { css } from "@emotion/css";
+
+export const HomeStyles = {
+  container: css`
+    background-color: var(--body-brackground-color);
+  `,
+};
